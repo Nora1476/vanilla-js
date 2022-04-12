793e7bbc54719aa6eb903bd4fd06b3270f24b506
Nora1476 <nada70979@gmail.com>
2022-04-12 10:07:48 +0900

    delete todos

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,12 +2,31 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(toDos)); /* → todo 배열이 추가되는거임 */
+}
+
+
+function deleteToto(event) {
+  const li = event.target.parentElement
+  li.remove();
+}
+
 
 function paintTodo(newTodo) {
    const li = document.createElement("li");
    const span = document.createElement("span");
-   li.appendChild(span);
    span.innerText = newTodo;
+   const button = document.createElement("button")
+   button.innerText="❌";
+   button.addEventListener("click", deleteToto)
+
+   li.appendChild(span);
+   li.appendChild(button);
+  
    toDoList.appendChild(li);
 
 }
@@ -17,7 +36,9 @@ function handleTodoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    paintTodo(newTodo);
+    toDos.push(newTodo);
+    paintTodo(newTodo);  /* 화면에 구현하는 함수*/
+    saveTodos();        /*local storage에 저장하는 함수(새로고침해도 사라지지않음)*/
 }
 
 toDoForm.addEventListener("submit", handleTodoSubmit)
